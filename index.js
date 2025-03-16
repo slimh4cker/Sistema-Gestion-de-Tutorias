cb7bd99049f6f5d5b3776ff75da506b4bfec77d2
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-16 16:03:43 -0700

    Servidor simple creado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Sistema gestion de asesorias");
+import express, { json } from 'express';
+
+
+export const createApp = () => {
+  const app = express();
+
+  app.disable("x-powered-by"); //Ocultar la cabecera de express
+
+  // Poner aqui middlewares
+
+  // Enrutadores aqui
+
+
+  //  configuracion de puerto
+  const PORT = process.env.PORT || 1234; // puerto default 1234
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+createApp();
