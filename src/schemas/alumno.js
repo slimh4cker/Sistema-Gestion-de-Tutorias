e91773e67606b27b7ca1465d85264b6090df4c9a
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-25 16:36:05 -0700

    agregada funcion validarParcialAlumno y agregado cometnarios

--- src/schemas/alumno.js
+++ src/schemas/alumno.js
@@ -17,6 +17,14 @@ const alumnoSchema = z.object({
 
 })
 
+// funcion para validar alumno si queremos que CUENTE CON TODOS LOS DATOS
+// retorna true o false
 export function validarAlumno(alumno) {
   return alumnoSchema.safeParse(alumno)
+}
+
+// funcion para validar alumno si NO NOS IMPORTA que cuente con todos los datos
+// retorna true o false
+export function validarParcialAlumno(alumno){
+  return alumnoSchema.partial().safeParse(alumno) 
 }
