d8682371e29f6dfe4ad09096412e1491f1f5de7c
diego <eldieguillo87@gmail.com>
2025-03-25 14:13:08 -0700

    integracion del gitignore, .env y avance del modelo

---
+++ src/models/AmazonRDS/Modelo_cuentas.js
@@ -0,0 +1,130 @@
+import { DataTypes } from "sequelize"; // Import the built-in data types
+import { sequelize } from "../../utils/database_connection.js"; // Import the connection object
+
+//Definicion del modelo de la tabla 'adminstradores'
+const modelo_cuenta_administrador = sequelize.define('modelo_cuenta_administrador',{
+    id: {
+        type: DataTypes.INTEGER(11),
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: null,
+    },
+    email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        defaultValue: null,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        defaultValue: null,
+    },
+    fecha_registro: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+},
+    {tableName: 'administradores',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+});
+
+// Definición del modelo de la tabla 'asesores'
+
+const modelo_cuenta_asesor = sequelize.define('modelo_cuenta_asesores',{
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: null
+    },
+    email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true,
+        defaultValue: null
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        defaultValue: null,
+    },
+    area_especializacion: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        defaultValue: null
+    },
+    disponibilidad: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        defaultValue: null,
+    },
+    fecha_registro: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: sequelize.literal('CURRENT_TIMESTAMP'),
+    }
+},
+{ tableName: 'asesores',
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
+}
+)
+
+// Definición del modelo de la tabla 'estudiantes'
+const modelo_cuenta_estudiante = sequelize.define('modelo_cuenta_estudiante', {
+    id: {
+        type: DataTypes.INTEGER(11),
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: null,
+    },
+    email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        defaultValue: null,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        defaultValue: null,
+    },
+    fecha_registro: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+},
+    { tableName: 'estudiantes',
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false
+    }
+)
+
+export {modelo_cuenta_estudiante};
+export {modelo_cuenta_administrador};
+export {modelo_cuenta_asesor};
