100eeeba9e9eceacaede95a94dcb60f5e11e6a31
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-25 23:01:06 -0700

    squema de alumno alterado

--- src/schemas/alumno.js
+++ src/schemas/alumno.js
@@ -2,9 +2,7 @@ import z from 'zod'
 
 const alumnoSchema = z.object({
   nombre: z.string().min(2).max(100),
-  curso: z.string().min(2).max(255),
   email: z.string().email().maxLength(150),
-  telefono: z.string().regex(/^\d{9}$/),
   password: z.string().min(8).max(255)
   .refine(password => {
     // Al menos una mayúscula
@@ -13,8 +11,6 @@ const alumnoSchema = z.object({
     const hasNumber = /[0-9]/.test(password);
     return hasUpperCase && hasNumber;
   }),
-  matricula: z.string().size(9),
-
 })
 
 // funcion para validar alumno si queremos que CUENTE CON TODOS LOS DATOS
