09e241e4d7b8bf8ab0f3df5e9d9884e3f52d5f1f
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-25 23:14:35 -0700

    Agregado modelo de asesor

---
+++ src/schemas/asesor.js
@@ -0,0 +1,27 @@
+import z from 'zod'
+
+const alumnoSchema = z.object({
+  nombre: z.string().min(2).max(100),
+  email: z.string().email().max(150),
+  password: z.string().min(8).max(255)
+  .refine(password => {
+    // Al menos una mayúscula
+    const hasUpperCase = /[A-Z]/.test(password);
+    // Al menos un número
+    const hasNumber = /[0-9]/.test(password);
+    return hasUpperCase && hasNumber;
+  }),
+  area_especializacion: z.string().min(2).max(100)
+})
+
+// funcion para validar alumno si queremos que CUENTE CON TODOS LOS DATOS
+// retorna true o false
+export function validarAlumno(alumno) {
+  return alumnoSchema.safeParse(alumno)
+}
+
+// funcion para validar alumno si NO NOS IMPORTA que cuente con todos los datos
+// retorna true o false
+export function validarParcialAlumno(alumno){
+  return alumnoSchema.partial().safeParse(alumno) 
+}
