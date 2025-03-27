d4656e03d2f75d2d1acd46150132c8d365f3c4b9
salva <salvadorlaguna155@gmail.com>
2025-03-26 17:59:05 -0700

    Import

--- src/routers/auth/RouterAsesor.js
+++ src/routers/auth/RouterAsesor.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { AsesorControler } from './src/controller/ControlerAsesor.js';
-import { verificarToken, requiereRol } from '../src/middlewares/auth.js';
+import { AsesorControler } from '../../controller/ControlerAsesor.js';
+import { verificarToken, requiereRol } from '../middlewares/auth.js';
 
 const router = express.Router();
 
