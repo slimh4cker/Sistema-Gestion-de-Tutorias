e03bf71b529a8772130de239e37263dcd63996cd
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-28 16:49:37 -0700

    Agregada funcion para crear solicitudes

--- src/controller/ControlerSolicitud.js
+++ src/controller/ControlerSolicitud.js
@@ -1,7 +1,7 @@
 /// Controler del recurso de asesorias
 // Utilizar cuando se quiere alterar algo de una asesoria
 import { SolicitudModel } from "../model/SolicitudModel.js"
-
+import { validarSolicitud } from "../schemas/solicitud.js"} 
 
 export class SolicitudControler {
   // Obtiene todas las solicitudes de un alumno en especifico
@@ -32,9 +32,27 @@ export class SolicitudControler {
   }
 
   // Crea una solicitud
-  static async crearSolicitudPorAlumno(req, res) {
+  static async crearSolicitudDeAlumno(req, res) {
+    const correo = obtenerMailDeReq(req)
 
+    datos = req.body
 
+    // validar solicitud
+    if (!validarSolicitud(datos)) {
+      res.status(400).json({ error: "Solicitud no valida" })
+      return
+    }
+
+    // crear solicitud
+    try {
+      const datos = SolicitudModel.crearSolicitudDeAlumno(correo, datos)
+    } catch (error) {
+      res.status(500).json({ error: "Error interno al crear solicitud" })
+      return
+    }
+
+    // enviar mensaje de que funciono correctamente
+    res.status(201).json({ message: "Solicitud creada correctamente" })
 
   }
 }
