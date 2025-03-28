fd27063f54835d5145f708324152261635056ddb
AndresBarraganM <al22760549@ite.edu.mx>
2025-03-28 16:51:36 -0700

    agregada validacion de rango de horas a solicitud

--- src/schemas/solicitud.js
+++ src/schemas/solicitud.js
@@ -7,13 +7,17 @@ niveles_urgencia = ['baja', 'media', 'alta']
 const solicitudSchema = z.object({
  tema: z.string().min(3).max(255),
  observaciones: z.string().max(255).optional(),
- fecha_limite: z.string().datetime(),
+ fecha_limite: z.string().datetime()
+  .refine((fecha) => {
+    const fechaLimite = new Date(fecha);
+    const ahora = new Date();
+    return fechaLimite > ahora;
+  }),
  modalidad: z.enum(modalidades),
  nivel_urgencia: z.enum(niveles_urgencia),
  estado: z.enum(estados).optional(),
 })
 
-// TODO probar que el formato de fechas sea correcto y que la fecha de atencion sea mayor a la fecha de creacion y otras validaciones de fechas
 export async function validarSolicitud(solicitud) {
   const resultado = solicitudSchema.safeParse(solicitud);
   return resultado.success; 
