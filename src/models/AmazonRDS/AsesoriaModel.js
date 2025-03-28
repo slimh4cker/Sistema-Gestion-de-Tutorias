f0f86468a7d7f69088f185dc7745d0c66f9566a9
diego <eldieguillo87@gmail.com>
2025-03-28 01:48:03 -0700

    Obtener asesorias por correo de alumno

--- src/models/AmazonRDS/AsesoriaModel.js
+++ src/models/AmazonRDS/AsesoriaModel.js
@@ -2,10 +2,11 @@ import { modelo_cuenta_estudiante } from "./Modelo_cuentas.js";
 import { modelo_asesorias, modelo_solicitudes } from "./Modelo_datos.js";
 
 export class AsesoriaModel{
+    // Método que obtiene las asesorias relacinandas al correo de un alumno
     static async getAseoriaByAlumnoEmail(email){
         const asesoria_alumno_email = await modelo_asesorias.findAll(email,{
-            include: [{
-                model: modelo_solicitudes,
+            include: [{ // Include es un INNER JOIN 
+                model: modelo_solicitudes, // Se 
                 include: [{
                     model: modelo_cuenta_estudiante,
                     where: {
