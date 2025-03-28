39b6552006859e82dd4f2c0f7b6d187cfb558892
salva <salvadorlaguna155@gmail.com>
2025-03-28 14:17:15 -0700

    Test

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 import express from 'express';
-import routerAsesor from './src/routers/auth/RouterAsesor.js'; // Ruta al archivo RouterAsesor.js
+import { authRouter } from './src/routers/auth/authRouter.js';
 
 export const createApp = () => {
   const app = express();
 
   app.disable("x-powered-by");
 
-  // Middlewares aquí
-  app.use(express.json()); // Asegúrate de tener esta línea para procesar JSON en las solicitudes
+  // Middlewares
+  app.use(express.json());
 
-  // Enrutadores aquí
-  app.use('/api', routerAsesor);
+  // Enrutadores
+  app.use('/auth', authRouter);
 
-  // Configuración de puerto
-  const PORT = process.env.PORT || 1234; // Puerto default 1234
+  // Ruta de prueba básica
+  app.get('/', (req, res) => {
+    res.send('¡API funcionando correctamente!');
+  });
 
+  // Manejo de rutas no existentes (404)
+  app.use((req, res) => {
+    res.status(404).json({ error: "Ruta no encontrada" });
+  });
+
+  const PORT = process.env.PORT || 1234;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
