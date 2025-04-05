c710946d70d2fbc1734a91cd2c6530ac9e2c6761
Diego <eldieguillo87@gmail.com>
2025-04-04 17:17:16 -0700

    Cambio en los metodos del usuario

--- src/models/AmazonRDS/AlumnoModel.js
+++ src/models/AmazonRDS/AlumnoModel.js
@@ -2,30 +2,47 @@ import { modelo_cuenta_estudiante } from "./Modelo_cuentas.js";
 
 // Clase del alumno encargada de interactuar con la base de datos
 
-export class AlumnoModel{
+export class AlumnModel{
     // Metodo para obtener un alumno por su correo
-    // Si no encuentra el alumno devuelve null
-    // de lo contrario retorna un diccionario con los datos del alumno
     static async getAlumnoByMail(email) {
-        const alumno = await modelo_cuenta_estudiante.findOne({
+        const correo_alumno = await modelo_cuenta_estudiante.findOne({
             where: {
-                email: email, // Unicamente se necesita buscar por correo
-                estado: 'activo' // Y estado debe ser igual a activo
-            },
-            raw: true
-        });
-    
-        return alumno || null;
-    }    
+                email: email,// unicamente se necesita buscar por correo
+                estado: 'activo' // y estado se igual a activo
+            }
+        })
+        return correo_alumno // Retorna un JSON con los datos del alumno
+    }
 
     static async createAlumno(datos) {
+        try {
+            const alumno = await modelo_cuenta_estudiante.findOne({
+                where: {
+                    email: datos.email
+                }
+            })
+            if(!alumno){
+                const crear_alumno = modelo_cuenta_estudiante.create({})
+                console.log("Alumno creado correctamente")
+                return crear_alumno
+            }
+            else if (alumno.dataValues.estado === 'inactivo'){
+                return this.reactivarAlumno(datos)
+            }
+            else if(alumno.dataValues.estado === 'activo'){
+                return null
+            }
+        }
+        catch(error){
+
+        }
         return await modelo_cuenta_estudiante.create(datos)
     }
 
-    static async updateAlumno(datos, emailOriginal) {
+    static async updateAlumno(datos) {
         return await modelo_cuenta_estudiante.update(datos, {
             where: {
-                email: emailOriginal
+                email: datos.email
             }
         })
     }
@@ -33,7 +50,22 @@ export class AlumnoModel{
     static async deleteAlumno(correo) {
         return await modelo_cuenta_estudiante.update({estado: 2},{
             where: {
-                email: correo
+                email: correo.email
+            }
+        })
+    }
+    static async reactivarAlumno (datos) {
+        console.log("Correo reactivado Correctamente")
+        return await modelo_cuenta_estudiante.update({
+            nombre: datos.nombre,
+            password: datos.password,
+            estado: 1
+
+        },
+        {
+            where: {
+                email: datos.email
+
             }
         })
     }
