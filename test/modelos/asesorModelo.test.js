8331c45f2812b705c58ae933f3619c1dfe74f37f
AndresBarraganM <al22760549@ite.edu.mx>
2025-04-08 10:29:19 -0700

    test asesor ahora prueba crear y elminar

--- test/modelos/asesorModelo.test.js
+++ test/modelos/asesorModelo.test.js
@@ -20,4 +20,32 @@ describe('validar asesor', () => {
       expect(resultado).toBe(null)
     })
   });
+
+  describe('crear y eliminar asesor', () => {
+    it('creando un asesor y eliminandolo despues', async () => {
+      const asesor = {
+        nombre: "juanita",
+        area_especializacion: "Redes de computadoras",
+        email: "temporal@juanita.com",
+        password: "Password123!",
+      }
+
+      // Crear el asesor
+      await AsesorModel.createAsesor(asesor)
+      
+      // verificar que existe
+      let resultado = await AsesorModel.getAsesorByMail(asesor.email)
+      expect(resultado).toEqual(expect.objectContaining(
+        asesor
+      ))
+
+
+      // eliminar el asesor
+      await AsesorModel.deleteAsesor(asesor.email)
+      
+      // verificar que no existe
+      let resultado_eliminar = await AsesorModel.getAsesorByMail(asesor.email)
+      expect(resultado_eliminar).toBe(null)
+    })
+  })
 })
