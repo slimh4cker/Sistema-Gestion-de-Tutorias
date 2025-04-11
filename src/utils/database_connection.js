5dc10cf96be9a3a071cc316d48e5b48f6d55b207
BrandonVincenzo <al22760550@ite.edu.mx>
2025-04-10 22:18:13 -0700

    DB connection (Me daba error )

--- src/utils/database_connection.js
+++ src/utils/database_connection.js
@@ -10,7 +10,7 @@ const { DB_NAME, USER, PASSWORD, HOST, PORT } = process.env;
 const sequelize = new Sequelize('asesorias', 'root', '1234',{
     host: 'localhost',  
     dialect: 'mysql',
-    port: 3306,
+    port: 3306,      
 });
 
 export { sequelize };
