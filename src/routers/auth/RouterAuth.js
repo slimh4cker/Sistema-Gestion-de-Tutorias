6dd742ab4c525cb56b04386483ac9ca97fbbb7eb
salva <salvadorlaguna155@gmail.com>
2025-04-15 15:10:25 -0700

    improv: rutas especificas para la autenticacion consolidadas en un mismo archivo

--- src/routers/auth/RouterAuth.js
+++ src/routers/auth/RouterAuth.js
@@ -2,11 +2,24 @@ import { Router } from 'express';
 import {loginEstudiante} from '../../controller/auth/estudianteAuth.js';
 import {loginAsesor} from '../../controller/auth/asesorAuth.js';
 import {loginAdmin} from '../../controller/auth/administradorAuth.js';
+import { authMiddleware } from '../utils/jwt/jwt.js';
+import { registrarAdmin } from '../../controller/auth/administradorAuth.js';
+import { registrarEstudiante } from '../../controller/auth/estudianteAuth';
+import { registrarAsesor } from '../../controller/auth/asesorAuth.js';
 
 const router = Router();
 
+// Login routes
 router.post('/auth/alumno', loginEstudiante);
 router.post('/auth/asesor', loginAsesor);
 router.post('/auth/administrador', loginAdmin);
+
+// Register routes
+router.post('/administrador',
+  authMiddleware(['administrador']),
+  registrarAdmin
+);
+router.post('/alumno', registrarEstudiante);
+router.post('/asesor', registrarAsesor);
 
 export default router;
