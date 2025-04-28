ab221f601142f69ba1357bbef8b882a120732bcf
salva <salvadorlaguna155@gmail.com>
2025-04-28 16:09:52 -0700

    dev: cambios menores

--- src/routers/RouterAlumno.js
+++ src/routers/RouterAlumno.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getAlumnoByMail } from '../../controller/auth/estudianteAuth';
+import { getAlumnoByMail, getSolicitudesPorEstado } from '../../controller/auth/estudianteAuth';
 
 const router = Router();
 
 router.get('/alumno/alumno', getAlumnoByMail);
+router.get('/alumno/solicitud', getSolicitudesPorEstado)
+router.post('/alumno')
 
 export default router;
