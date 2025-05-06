043b72dad5eb4ca8de3cb9e1da3d56f0642af995
Alexia Montelongo <montelongoalexia@gmail.com>
2025-05-06 02:00:15 -0700

    arreglo de carpetas en view/frontend

--- src/view/frontend/js/navbar.js
+++ src/view/frontend/js/navbar.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbarPlaceholder = document.getElementById('navbar-placeholder');
 
     if (navbarPlaceholder) {
-        fetch('components/navbar.html')
+        fetch('../components/navbar.html')
             .then(response => response.text())
             .then(data => {
                 navbarPlaceholder.innerHTML = data;
--- src/view/frontend/js/navbar_asesor.js
+++ src/view/frontend/js/navbar_asesor.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbarPlaceholder = document.getElementById('navbar-placeholder');
 
     if (navbarPlaceholder) {
-        fetch('components/navbar_asesor.html')
+        fetch('../components/navbar_asesor.html')
             .then(response => response.text())
             .then(data => {
                 navbarPlaceholder.innerHTML = data;
