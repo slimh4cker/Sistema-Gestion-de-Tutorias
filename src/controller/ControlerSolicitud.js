eea0b1d891b5ad429caa5764780aa98aea906a86
AndresBarraganM <al22760549@ite.edu.mx>
2025-05-06 17:19:14 -0700

    refactor: improve error handling and code structure in SolicitudControler

--- src/controller/ControlerSolicitud.js
+++ src/controller/ControlerSolicitud.js
@@ -1,22 +1,21 @@
 /// Controler del recurso de asesorias
 // Utilizar cuando se quiere alterar algo de una asesoria
-import { SolicitudModel } from "../model/SolicitudModel.js"
+import { SolicitudModel } from "../models/AmazonRDS/SolicitudModel.js"
 import { validarSolicitud } from "../schemas/solicitud.js"
+import { obtenerIdDeReq } from "../utils/request.js"
 
 export class SolicitudControler {
   // Obtiene todas las solicitudes de un alumno en especifico
   static async getTodasSolicitudesDeAlumno(req, res) {
-    const correo = obtenerMailDeReq(req)
-
     try {
-      const datos = SolicitudModel.getSolicitudesDeAlumno(correo)
-    } catch (s) {
+      const correo = obtenerMailDeReq(req)
+      const datos = await SolicitudModel.getSolicitudesDeAlumno(correo)
+      res.status(200).json(datos)   
+        
+    } catch (e) {
       res.status(500).json({ error: "Error interno al buscar solicitudes del alumno" })
       return
     }
-    
-
-    res.status(200).json(datos)
   }
 
   // Obtiene las solicitudes que no han sido asignadas a ningun asesor
@@ -31,21 +30,33 @@ export class SolicitudControler {
     res.status(200).json(datos)
   }
 
-  // Crea una solicitud
-  static async crearSolicitudDeAlumno(req, res) {
-    const correo = obtenerMailDeReq(req)
+  /*
+   Crea una solicitud
+   Los datos que envia son lo siguiente:
+   {
+     "modalidad": "en_linea",
+     "tema": "calculo integral",
+     "fecha_limite": "13-4-2025",
+     "observaciones": "ninguna",
+     "estudiante_id": 5
+   }
+   y el id es el obtenido en el token
+  */
 
-    datos = req.body
+  static async crearSolicitudDeAlumno(req, res) {
+    const id = obtenerIdDeReq(req)
+    const datos = req.body
+    const datos_completos = (datos.estudiante_id = id)
 
     // validar solicitud
-    if (!validarSolicitud(datos)) {
+    if (!validarSolicitud(datos_completos)) {
       res.status(400).json({ error: "Solicitud no valida" })
       return
     }
 
     // crear solicitud
     try {
-      const datos = SolicitudModel.crearSolicitudDeAlumno(correo, datos)
+      await SolicitudModel.agregarSolicitud(datos)
     } catch (error) {
       res.status(500).json({ error: "Error interno al crear solicitud" })
       return
