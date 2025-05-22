c99ad49b9fdf97ad2468749671411531521e92a9
AndresBarraganM <al22760549@ite.edu.mx>
2025-05-22 14:10:33 -0700

    tooltips de correo creadas

--- src/utils/mail/sendmail.js
+++ src/utils/mail/sendmail.js
@@ -13,6 +13,28 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+/**
+ * Envía un correo electrónico al destinatario usando una plantilla HTML personalizada.
+ *
+ * La función selecciona una plantilla de correo según el tipo especificado, reemplaza
+ * los marcadores con los datos proporcionados y envía el correo usando el transportador SMTP.
+ * Asegurese que el campo "plantilla" sea correspondiente a una de las plantillas en la carpeta de plantillas dentro de src/utils/mail/plantillas.
+ *
+ * @async
+ * @function sendMail
+ * @param {string} destinatario - Dirección de correo electrónico del receptor.
+ * @param {string} plantilla - Nombre de la plantilla a usar (por ejemplo: "asesoriaAsignadaAlumno").
+ * @param {object} datos - Objeto con los datos a insertar en la plantilla (nombre, fecha, hora, etc).
+ * @returns {Promise<string>} - Promesa que se resuelve con la respuesta del envío o se rechaza con un error.
+ *
+ * @example
+ * await sendMail("usuario@example.com", "asesoriaAsignadaAlumno", {
+ *   nombre_asesoria: "Matemáticas",
+ *   nombre_asesor: "Juan Pérez",
+ *   fecha: "2025-05-23",
+ *   hora: "10:00 AM"
+ * });
+ */
 export async function sendMail(destinatario, plantilla, datos ) {
   return new Promise((resolve, reject) => {
 
